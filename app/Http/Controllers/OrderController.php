f1bb6944e97004ee1b10ae5688b2c6e46a0a7e5b
Bùi Quốc Vũ <bui@192.168.0.133>
2024-12-24 17:01:31 +0700

    pull

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -4,6 +4,9 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Exports\OrderTiktokExport;
+use App\Imports\OrderTiktokimport;
+
+
 use Maatwebsite\Excel\Facades\Excel;
 
 
--- app/Models/Order.php
+++ app/Models/Order.php
@@ -8,4 +8,43 @@ use Illuminate\Database\Eloquent\Model;
 class Order extends Model
 {
     use HasFactory;
+
+    // Các trường cho phép gán hàng loạt
+    protected $fillable = [
+        'order_code',             
+        'status',                   
+        'createdorder_at',        
+        'product_code',             // ma_san_pham
+        'quantity',                 // so_luong
+        'salework_price',           // gia_salework
+        'product_name', 
+        'soluongban_tiktok',            // ten_san_pham
+        'marketplace_price',        // gia_san_tmdt
+        'estimated_revenue',        // doanh_thu_uoc_tinh
+        'discount',                 // chiet_khau
+        'prepaid_amount',           // khach_tra_truoc
+        'shipping_fee',             // phi_van_chuyen
+        'customer_name',            // ten_khach_hang
+        'phone_number',             // so_dien_thoai
+        'shipment_code',            // ma_van_don
+        'carrier',                  // don_vi_van_chuyen
+        'address',                  // dia_chi
+        'province',                 // tinh
+        'district',                 // huyen
+        'ward',                     // xa
+        'customer_note',            // ghi_chu_cua_khach
+        'shop_name',                // shop
+        'creator',                  // nguoi_tao_don
+        'cod_amount',               // tien_thu_ho
+        'shipping_fee_collected',   // phi_ship
+        'total_value',              // tong_gia_tri
+        'order_processing_status',  // xu_ly_don_hang
+        'processing_time',          // thoi_gian_xu_ly
+        'packed',                   // dong_hang
+        'packing_time',             // thoi_gian_dong
+        'shipped',                  // gui_hang
+        'shipping_time',            // thoi_gian_gui
+        'warehouse',                // kho_hang
+        'promotion_by_seller',      // nguoi_ban_khuyen_mai
+    ];
 }
