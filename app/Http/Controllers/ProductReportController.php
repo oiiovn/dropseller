3fff1337e5804ee66b634597276c3ac53a71ac57
Bùi Quốc Vũ <bui@192.168.1.9>
2025-01-04 01:20:36 +0700

    ẩn thông báo

---
+++ app/Http/Controllers/ProductReportController.php
@@ -0,0 +1,26 @@
+﻿<?php
+
+namespace App\Http\Controllers;
+
+use App\Services\ProductReportService;
+use Illuminate\Http\Request;
+
+class ProductReportController extends Controller
+{
+    protected $productReportService;
+
+    public function __construct(ProductReportService $productReportService)
+    {
+        $this->productReportService = $productReportService;
+    }
+
+    public function index(Request $request)
+    {
+        $timeStart = $request->input('time_start');
+        $timeEnd = $request->input('time_end');
+
+        $report = $this->productReportService->getProductReport($timeStart, $timeEnd);
+
+        return view('product.report', compact('report'));
+    }
+}
---
+++ resources/views/product/report.blade.php
@@ -0,0 +1,31 @@
+﻿@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <h1>Product Report</h1>
+    @if(isset($report['data']['product_report']))
+        <table class="table">
+            <thead>
+                <tr>
+                    <th>Product ID</th>
+                    <th>Product Name</th>
+                    <th>Quantity Sold</th>
+                    <th>Total Revenue</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach($report['data']['product_report'] as $product)
+                    <tr>
+                        <td>{{ $product['product_id'] }}</td>
+                        <td>{{ $product['product_name'] }}</td>
+                        <td>{{ $product['quantity_sold'] }}</td>
+                        <td>{{ $product['total_revenue'] }}</td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+    @else
+        <p>No data available for the selected time period.</p>
+    @endif
+</div>
+@endsection
