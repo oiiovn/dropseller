80cc96206a67e26f04599571896f8c290ad5ca42
Bùi Quốc Vũ <bui@192.168.1.6>
2025-01-15 16:21:07 +0700

    show shop

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -4,12 +4,24 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use App\Models\Shop;
+use App\Models\Order;
 
 class ProfileController extends Controller
 {
     public function viewProfile()
-    {
-        $user = Auth::user(); // Lấy thông tin người dùng hiện tại
-        return view('profile.profile', compact('user'));
+{
+    $user = Auth::user();
+    $shops = Shop::where('user_id', $user->id)->get();
+
+    foreach ($shops as $shop) {
+        $shop->revenue = Order::where('shop_id', $shop->shop_id)->sum('total_bill'); 
+        $orders = Order::where('shop_id', $shop->shop_id)->get();
     }
+    return view('profile.profile', compact('user', 'shops'));
+}
+
+    
+    
+
 }
