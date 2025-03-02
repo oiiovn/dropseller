86ea3b33d6b99a2bc52caa1082de72d992616fbd
Bùi Quốc Vũ <>
2025-03-02 11:12:33 +0700

    push

--- resources/views/profile/profile.blade.php
+++ resources/views/profile/profile.blade.php
@@ -337,7 +337,7 @@
                                                 @if($shop->platform == 'Tiktok')
                                                     <img src="https://img.icons8.com/ios-filled/250/tiktok--v1.png" alt="" style="width: 50px; height: 50px;">
                                                     @elseif($shop->platform == 'Shopee')
-                                                    <img src="https://res.cloudinary.com/dup7bxiei/image/upload/v1740886059/avatars/n3hv3omjxgvebrjey2rv.png" alt="" style="width: 50px; height: 50px;">
+                                                    <img src="https://img.icons8.com/fluency/240/shopee.png" alt="" style="width: 50px; height: 50px;">
                                                     @else
                                                     <i class="fas fa-store me-1"></i>
                                                     @endif
