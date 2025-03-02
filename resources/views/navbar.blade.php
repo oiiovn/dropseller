a8e85faa8858dc6e9cb939f4acd7be9d561b1b09
Bùi Quốc Vũ <>
2025-03-02 11:23:20 +0700

    push

--- resources/views/navbar.blade.php
+++ resources/views/navbar.blade.php
@@ -4,10 +4,10 @@
         <!-- Light Logo -->
         <a href="{{route('dashboard')}}" class="logo logo-light">
             <span class="logo-sm">
-                <img src="assets/images/logo-sm.png" alt="" height="22">
+                <img src="https://img.icons8.com/windows/512/FFFFFF/blog-logo.png" alt="" height="25">
             </span>
             <span class="logo-lg">
-                <img src="assets/images/logo-light.svg" alt="" height="57">
+                <img src="https://img.icons8.com/windows/512/FFFFFF/blog-logo.png" alt="" height="60">
             </span>
         </a>
         <button type="button" class="btn btn-sm p-0 fs-20 header-item float-end btn-vertical-sm-hover" id="vertical-hover">
