2486167794106671baf8c0f12583a0fec6b645da
Bùi Quốc Vũ <>
2025-08-22 08:57:55 +0700

    Đổi tài khoản ngân hàng

--- app/Console/Commands/FetchTransactions.php
+++ app/Console/Commands/FetchTransactions.php
@@ -27,8 +27,8 @@ class FetchTransactions extends Command
         $token = base64_encode($secretKey);
 
         $requestBody = [
-            "bankAccounts" => "PHATLOC934584939", // Số tài khoản chính xác
-            "begin" => "21/08/2025",        // Ngày bắt đầu 
+            "bankAccounts" => "46241987", // Số tài khoản chính xác
+            "begin" => "22/08/2025",        // Ngày bắt đầu 
             "end" => "20/11/2029"          // Ngày kết thúc
         ];
 
